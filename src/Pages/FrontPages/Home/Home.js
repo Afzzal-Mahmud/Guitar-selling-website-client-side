41d8230c7259d79aca1ce23a22227a758cf54512
afzal mahmud <afzzalmahmud@gmail.com>
2022-01-04 11:03:00 -0800

    add hero section of website and make some adjustment.

--- src/Pages/FrontPages/Home/Home.js
+++ src/Pages/FrontPages/Home/Home.js
@@ -1,8 +1,24 @@
+import Container from "react-bootstrap/Container";
+
+/* import home style */
+import './Home.css'
+
 function Home() {
     return(
-        <h2>
-            hello form home
-        </h2>
+        <Container className='hero-container'>
+            <div className='hero-main'>
+                <p className='color-gray poppins-Regular'>EXPART DESIGNER MAKE YOUR GUITAR !</p>
+                <h1 className='poppins-bold hero-title'>Some grate instrument <br /> comes with grate <br /> experience .</h1>
+                <div className="hero-btn poppins-bold">
+                    <button className='primary-background explore-btn'>
+                        Explore More</button>
+                    <button>Contact Us</button>
+                </div>
+            </div>
+            <div className='hero-image'>
+                <img src="https://i.ibb.co/87tjLph/guitar-home-removebg-preview.png"/>
+            </div>
+        </Container>
     )
 }
 export default Home;
