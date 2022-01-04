a83c9a6366eb108c50c3331f180ef9c8a06cd290
afzal mahmud <afzzalmahmud@gmail.com>
2022-01-04 09:48:56 -0800

    add some header style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {BrowserRouter as Router, Link, Switch, Route, BrowserRouter} from 'react-router-dom';
 
-import Home from './FrontPages/Home/Home';
+import Home from './Pages/FrontPages/Home/Home';
 import Header from './Components/Header/Header';
 
 function App() {
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import  Nav from "react-bootstrap/Nav";
 import  Navbar from "react-bootstrap/Navbar";
 import  Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button"
+import './Header.css'
 
 /* import font */
 import '../../UtilityCss/UtilityCss.css'
@@ -10,13 +11,13 @@ function Header() {
     return(
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
 
-  <Container>
+  <Container className='py-3'>
       {/* logo */}
-  <Navbar.Brand href="#home" className='poppins-boldItalic primary-forground'>Guitar</Navbar.Brand>
+  <Navbar.Brand href="#home" className='poppins-boldItalic logo primary-forground'>Guitar</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   {/* nav item */}
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="ms-auto poppins-regular">
+    <Nav className="ms-auto poppins-regular nav-links">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#explore">Explore</Nav.Link>
       <Nav.Link href="#contact">Contact Us</Nav.Link>
