8d85a3889149174c15898dbd4bbbeaed9ffbd40a
afzal mahmud <afzzalmahmud@gmail.com>
2022-01-06 22:20:11 -0800

    add our Unique product collection list on home page

--- src/Pages/FrontPages/UniqueCollections.js/UniqueCollectionCards.js
+++ src/Pages/FrontPages/UniqueCollections.js/UniqueCollectionCards.js
@@ -1,6 +1,18 @@
-function UniqueCollectionCards() {
+function UniqueCollectionCards(props) {
+    const {name,image,price,description} = props.guitarObject
     return(
-        <h2>card working</h2>
+        <>
+        <div className="card-body">
+            <img className="card-image" src={image}/>
+
+            <div className="card-content">
+                <h3 className="poppins-semiBold secondery-forground">{name}</h3>
+                <h4 className="poppins-semiBold secondery-black">Rs. {price}</h4>
+                <p>{description}</p>
+                <button className="primary-background poppins-bold">Buy Now</button>
+            </div>
+        </div>
+        </>
     )
 }
 export default UniqueCollectionCards
--- src/Pages/FrontPages/UniqueCollections.js/UniqueCollections.js
+++ src/Pages/FrontPages/UniqueCollections.js/UniqueCollections.js
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 
 import UniqueCollectionCards from "./UniqueCollectionCards";
+/* import css for some styling */
+import './UniqueCollections.css'
+
 
 function UniqueCollections() {
     /* fetching data from public folder.At the end of the project it would be the mongoDb data base*/
@@ -19,14 +22,24 @@ function UniqueCollections() {
     },[])
     return(
         /* unique collections.js file is all about fetching data. All content is grow on uniqueCollectionCard.js file */
-        <Container>
-            <h2 className="poppins-boldItalic">Our Unique Collections</h2>
-            {
+        <Container className="pt-4">
+            <h2 className="poppins-boldItalic secondery-forground">
+            Our Unique Collections</h2>
+
+            <p className="poppins-regular color-gray-2 collection-sub-heading">We have some unique collection for you.Check it out</p>
+            
+        {/* the parent container for card */}
+
+        <div className="card-container">
+
+        {
                 uniqueGuitar.map(guitarData => <UniqueCollectionCards
                 key={guitarData.uk}
                 guitarObject={guitarData}
                 ></UniqueCollectionCards>)
-            }
+        }
+
+        </div>
         </Container>
     )
 }
