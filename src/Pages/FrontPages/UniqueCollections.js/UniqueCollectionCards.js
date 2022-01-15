679039f1d53f6917d14ba8aa0e942288f7f96e8f
afzal mahmud <afzzalmahmud@gmail.com>
2022-01-15 08:25:47 -0800

    send front-page uniqueGuitarCollection to the database

--- src/Pages/FrontPages/UniqueCollections.js/UniqueCollectionCards.js
+++ src/Pages/FrontPages/UniqueCollections.js/UniqueCollectionCards.js
@@ -1,6 +1,43 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import useAuth from "../../../Hooks/useAuth";
 function UniqueCollectionCards(props) {
+    const {user} = useAuth();
+    const userEmail = user.email
     const {name,image,price,description} = props.guitarObject
+
+    const handleAddToCart = () =>{
+        const orderItem = {
+            name : name,
+            imageUrl : image,
+            productPrice : price,
+            email : userEmail,
+            productType : 'Acoustic Unique Guitar'
+        }
+        console.log(orderItem)
+
+        /* send product data to the server */
+        fetch('http://localhost:5000/cart',{
+            method : "POST",
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(orderItem)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.insertedId){
+                    alert('Your product add successfully.See that on Dashboard page')
+                }
+            })
+    }
+
+    /* if user is not logIn redirect to logIn page */
+    const history = useHistory()
+    const handleLogInRedirect = () =>{
+        history.push('/login')
+    }
     return(
         <>
         <div className="guitar-card-body">
@@ -10,7 +47,14 @@ function UniqueCollectionCards(props) {
                 <h3 className="poppins-semiBold secondery-forground">{name}</h3>
                 <h4 className="poppins-semiBold secondery-black">Rs. {price}</h4>
                 <p>{name}, {description}</p>
-                <button className="primary-background poppins-bold">Buy Now</button>
+
+                {/* apply condition to see deshboard */}
+                {
+                        userEmail ?
+                        <button className="primary-background poppins-semiBold" onClick={handleAddToCart}>Add to Cart</button> 
+                        : 
+                        <button className="primary-background poppins-semiBold" onClick={handleLogInRedirect}>Add to Cart</button>
+                    }
             </div>
         </div>
         </>
